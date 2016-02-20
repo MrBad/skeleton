bcfc40dd93048120a060c7b95d53b3aef622a2a3
MrBadNews <viorel.irimia@gmail.com>
2016-02-20 13:46:52 +0200

    new

---
+++ create_tmp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#	This file is creating initial tmp (temporary) directory structure
+#
+mkdir -p tmp/templates_c/main/
+mkdir -p tmp/cache/describes/
+mkdir -p tmp/cache/smarty/main/
+mkdir -p tmp/logs/
+mkdir -p tmp/pids/
+
+chmod 777 tmp -R
