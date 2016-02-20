331801f38e2ae4aaf43fbb6015b46fb25287b4f3
MrBadNews <viorel.irimia@gmail.com>
2016-02-20 13:23:58 +0200

    removed some particular conf

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ exit
 rsync -n -varlogpt --progress --exclude="include/conf.php" --exclude="tmp" \
 --exclude=".htpassw" --exclude=".htaccess" --exclude="htdocs/admin" \
 --exclude="htdocs/upload" \
-/home/develop/eprimar/* root@uk:/home/eprimar/
+/home/develop/skeleton/* root@uk:/home/skeleton/
 
 #ssh uk "/etc/init.d/php5-fpm reload; /etc/init.d/apache2 reload;"
